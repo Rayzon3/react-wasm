bca152e57b357d30fec2f9100a47f9b6b4a4c728
Rahul Bhardwaj ツ <51912472+Rayzon3@users.noreply.github.com>
2023-09-08 08:42:35 +0530

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,25 @@ import "./App.css";
 
 
 function App() {
+
+  const [digitOne, setdigitOne] = useState(0)
+  const [digitTwo, setdigitTwo] = useState(0)
   const [ans, setAns] = useState(0);
 
   useEffect(() => {
     init().then(() => {
-      setAns(add(1, 1));
+      setAns(add(digitOne, digitTwo));
     });
-  }, []);
+  }, [digitOne, digitTwo]);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>1 + 1 = {ans}</p>
+        {/* <p>1st number:</p> */}
+        <input placeholder="1st Number" type="number" onChange={(e) => setdigitOne(e.target.value as unknown as number)} />
+        <input placeholder="2nd Number" type="number" onChange={(e) => setdigitTwo(e.target.value as unknown as number)} />
+        <p>{digitOne} + {digitTwo} = {ans}</p>
       </header>
     </div>
   );
