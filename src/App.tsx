5928f57ce09d8b9fdf77623f066f633c0b87da0e
Rahul Bhardwaj ツ <51912472+Rayzon3@users.noreply.github.com>
2023-09-08 08:43:12 +0530

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>1st number:</p> */}
         <input placeholder="1st Number" type="number" onChange={(e) => setdigitOne(e.target.value as unknown as number)} />
         <input placeholder="2nd Number" type="number" onChange={(e) => setdigitTwo(e.target.value as unknown as number)} />
         <p>{digitOne} + {digitTwo} = {ans}</p>
